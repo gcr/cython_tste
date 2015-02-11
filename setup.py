225af7296cdcc6d7660ae31198f3a9a5f9cd60ee
Michael Wilber <wilmic1102@gmail.com>
2015-02-11 10:54:47 -0500

    No more march=native

--- setup.py
+++ setup.py
@@ -8,7 +8,12 @@ import numpy
 setup(name='tste_cy',
       ext_modules = cythonize(Extension('cy_tste',["cy_tste.pyx",],
                                         include_dirs = [numpy.get_include()],
-                                        extra_compile_args = ['-fopenmp', '-O3', '-ffast-math', '-march=native'],
+                                        extra_compile_args = [
+                                            '-fopenmp',
+                                            '-O3',
+                                            '-ffast-math',
+                                            #'-march=native',
+                                            ],
                                         extra_link_args = ['-fopenmp'],
                   )),
       description="Cython flavor of TSTE",
